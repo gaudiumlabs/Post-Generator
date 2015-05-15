a2b1011510eb5fbe3f8fffbce71305275c4a828f
Gaudium org <gaudiumlabs@gmail.com>
2015-05-14 23:00:44 -0400

    added comments
    added comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -157,6 +157,7 @@ public class Main {
 		System.out.println("---EnD oF PrOgRaM---");
 		
 		//FOLLOW THE TUTORIAL AT http://www.vogella.com/tutorials/MySQLJava/article.html"
+		//READ "DatabaseLearning.txt notes under main.Notes package.
 		System.out.println("SQL testing, if your mySQL database is setup, this should"
 				+ "return a valid sql response. If not, you will get a sql error\n"
 				+ " Edit the test Sql function to play with it");
