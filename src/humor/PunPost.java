84e79b4d1bf93df9f01fa3e3b0338067e9259fe8
Dustin <dustin.td@gmail.com>
2015-05-21 08:33:45 -0400

    standup test joke gen is working

---
+++ src/humor/PunPost.java
@@ -0,0 +1,15 @@
+package humor;
+
+/**
+ * This is a fun side experiment to see if I can create a punny post given one or more keywords.
+ * 
+ * @author dustin
+ *
+ */
+public class PunPost {
+
+	public String generatePunPost(String[] keywords) {
+		return "";
+	}
+	
+}
---
+++ src/humor/TestJokeGen.java
@@ -0,0 +1,46 @@
+package humor;
+
+import standup.joke.*;
+import standup.profiling.ProfileException;
+import standup.profiling.ProfileManager;
+import standup.utils.CommandLineArguments;
+
+/**
+ * A simple program that shows how to use the STANDUP riddle generator API functionality.
+ * <p>
+ * You may have to specify a larger heap size for the JVM, e.g.
+ * <p>
+ * {@code java -Xms384m -Xmx384m TestJokeGen}
+ * 
+ * @author <a href="mailto:ruli.manurung@ed.ac.uk">Ruli Manurung</a>
+ */
+public class TestJokeGen
+{
+
+    public static void main(String[] args) throws ProfileException,GeneratorException
+    {
+        // Process any command line arguments
+        CommandLineArguments.parseCommandLineArguments(args);
+
+        // Create a new Backend
+        Backend myBackend = new BackendJokeSetOnly();
+        
+        // Initialize the profile manager and create a new profile
+        ProfileManager.initialize();
+        ProfileManager.createUser("username",myBackend);
+        ProfileManager.useProfile("username",myBackend);
+        
+        // Generate a 'new' joke
+        JokeStructure myJoke = myBackend.getNewJoke();
+        
+        // Print out the question and answer
+        System.out.println(myJoke.getQuestion());
+        System.out.println(myJoke.getAnswer());
+        
+        // Add the joke to the user's favourites
+        myBackend.addToFavourites(myJoke);
+        
+        // Save the profile
+        ProfileManager.saveCurrentProfile(myBackend);
+    }
+}
